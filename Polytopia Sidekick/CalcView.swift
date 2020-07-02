6a1320d359e67a9310afc55cad2681ed6cc01863
Ray Sun <raysun@gmail.com>
2020-07-01 17:46:12 -0700

    Making smaller vars for each UI section to describe them

--- Polytopia Sidekick/CalcView.swift
+++ Polytopia Sidekick/CalcView.swift
@@ -15,85 +15,75 @@ struct OptimizationValue {
 
 struct CalcView: View {
     @EnvironmentObject private var userData: UserData
-
+    
     @State var isBeforeCalculating = true
     @State var defender: Troop? = nil
-
+    
     let rows = Row.all()
     let troopWidth = CGFloat(70.0)
-
-    var body: some View {
-        VStack {
-            if (isBeforeCalculating) {
-
-                // Troop Picker
-
-                ForEach(rows) { row in
-                    HStack(alignment: .center) {
-                        ForEach(row.cells) { troop in
-                            Image(troop.imageURL)
-                                .resizable()
-                                .frame(width: self.troopWidth, height: self.troopWidth)
-                                .clipShape(Circle())
-                                .overlay(
-                                    Circle().stroke(Color.white, lineWidth: 2))
-                                .shadow(radius: 2)
-                                .onTapGesture {
-                                    if self.userData.defenders.count == 0 {
-                                        self.userData.defenders.append(troop.copy())
-                                    } else {
-                                        self.userData.attackers.append(troop.copy())
-                                    }
+    
+    var TroopPicker: some View {
+        ForEach(rows) { row in
+            HStack(alignment: .center) {
+                ForEach(row.cells) { troop in
+                    Image(troop.imageURL)
+                        .resizable()
+                        .frame(width: self.troopWidth, height: self.troopWidth)
+                        .clipShape(Circle())
+                        .overlay(
+                            Circle().stroke(Color.white, lineWidth: 2))
+                        .shadow(radius: 2)
+                        .onTapGesture {
+                            if self.userData.defenders.count == 0 {
+                                self.userData.defenders.append(troop.copy())
+                            } else {
+                                self.userData.attackers.append(troop.copy())
                             }
-                        }
                     }
                 }
-                Divider()
-
-                // Defender
-
-                Text(userData.defenders.count == 0 ? "Choose Defender" : "Defender")
-                    .bold()
-                ForEach(0..<userData.defenders.count, id: \.self) { i in
-                    TroopView(troop: self.$userData.defenders[i])
+            }
+        }
+    }
+    var Defender: some View {
+        VStack {
+            Text(userData.defenders.count == 0 ? "Choose Defender" : "Defender")
+                .bold()
+            ForEach(0..<userData.defenders.count, id: \.self) { i in
+                TroopView(troop: self.$userData.defenders[i], isDefender: true)
+                    .padding()
+            }
+        }
+    }
+    var Attackers: some View {
+        VStack {
+            Text(userData.defenders.count == 0 ? "" : userData.attackers.count == 0 ? "Choose Attackers" : "Attackers")
+                .bold()
+            ScrollView() {
+                ForEach(0..<userData.attackers.count, id: \.self) { i in
+                    TroopView(troop: self.$userData.attackers[i])
+                        .padding([.leading, .trailing])
+                }
+            }.frame(maxHeight: .infinity)
+        }
+    }
+    var BestAttacks: some View {
+        VStack {
+            Text("Defender").bold()
+            OptimalTroopView(troop: self.defender!)
+                .padding()
+                .frame(maxHeight: troopWidth * 1.2)
+            Divider()
+            Text("Optimal Attack Order").bold()
+            ScrollView() {
+                ForEach(userData.optimalTroops) { troop in
+                    OptimalTroopView(troop: troop)
                         .padding()
                 }
-                Divider()
-
-                // Attackers
-
-                Text(userData.defenders.count == 0 ? "" : userData.attackers.count == 0 ? "Choose Attackers" : "Attackers")
-                    .bold()
-                ScrollView() {
-                        ForEach(0..<userData.attackers.count, id: \.self) { i in
-                            TroopView(troop: self.$userData.attackers[i])
-                                .padding()
-                        }
-                }.frame(maxHeight: .infinity)
-
-            } else {
-
-                // Optimal Attack Results
-
-                Divider()
-                Text("Defender").bold()
-                OptimalTroopView(troop: self.defender!)
-                    .padding()
-                    .frame(maxHeight: troopWidth * 1.2)
-                Divider()
-                Text("Optimal Attack Order").bold()
-                ScrollView() {
-                    if (userData.optimalTroops.count > 0) {
-                        ForEach(userData.optimalTroops) { troop in
-                            OptimalTroopView(troop: troop)
-                            .padding()
-                        }
-                    }
-                }.frame(maxHeight: .infinity)
-            }
-
-            // Buttons
-
+            }.frame(maxHeight: .infinity)
+        }
+    }
+    var Buttons: some View {
+        VStack {
             if (userData.attackers.count > 0) {
                 HStack(alignment: .center, spacing: 25.0) {
                     Button("Calculate") {
@@ -112,6 +102,21 @@ struct CalcView: View {
                     }
                 }
             }
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            if (isBeforeCalculating) {
+                TroopPicker
+                Divider()
+                Defender
+                Divider()
+                Attackers
+            } else {
+                BestAttacks
+            }
+            Buttons
         }
         .animation(.easeInOut)
     }
